b11a20031c68f1c925f38a70ff3cb0a12d6ff357
dan-goodrick <dannyjeee@yahoo.com>
2023-08-30 16:04:00 -0600

    implements model relationships

--- src/model.js
+++ src/model.js
@@ -66,7 +66,34 @@ Movie.init(
   }
 );
 
+export class Rating extends Model {
+  [util.inspect.custom]() {
+    return this.toJSON();
+  }
+}
+Rating.init(
+
+  {
+    ratingId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    score: {
+      type:DataTypes.INTEGER,
+    },
+  },{
+    sequelize:db,
+    timestamps: true,
+    updatedAt: false
+  }
+)
+
+User.hasMany(Rating, {foreignKey: 'userId'})
+Rating.belongsTo(User, {foreignKey: 'userId'})
+Movie.hasMany(Rating, {foreignKey: 'movieId'})
+Rating.belongsTo(Movie, {foreignKey: 'movieId'})
 
 // const {User, db} = await import('./src/model.js');
-// db.sync()
+// db.sync({force: true})
 // > const testUser = await User.create({email: "test.email.com", password: 'test'});
