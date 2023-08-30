42fe14d8e450ab2ce3483bc3d1d7b4d0962aad6e
dan-goodrick <dannyjeee@yahoo.com>
2023-08-30 15:42:54 -0600

    finishes movie table

--- src/model.js
+++ src/model.js
@@ -1,8 +1,8 @@
-import { DataTypes, Model } from 'sequelize';
-import util from 'util';
-import connectToDB from './db.js';
+import { DataTypes, Model } from "sequelize";
+import util from "util";
+import connectToDB from "./db.js";
 
-export const db = await connectToDB('postgresql:///ratings');
+export const db = await connectToDB("postgresql:///ratings");
 
 export class User extends Model {
   [util.inspect.custom]() {
@@ -24,11 +24,49 @@ User.init(
     },
     password: {
       type: DataTypes.STRING,
-      allowNull: false
-    }
+      allowNull: false,
+    },
   },
   {
-    modelName: 'user',
+    modelName: "user",
     sequelize: db,
-  },
+  }
 );
+
+export class Movie extends Model {
+  [util.inspect.custom]() {
+    return this.toJSON();
+  }
+}
+
+Movie.init(
+  {
+    movieId: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    overview: {
+      type: DataTypes.TEXT,
+    },
+    releaseDate: {
+      type: DataTypes.DATE,
+    },
+    posterPath: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    modelName: "movie",
+    sequelize: db,
+  }
+);
+
+
+// const {User, db} = await import('./src/model.js');
+// db.sync()
+// > const testUser = await User.create({email: "test.email.com", password: 'test'});
